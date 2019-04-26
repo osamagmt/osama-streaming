b6f2de75652cbf3fcab6bca39ec5765191defb74
osamagmt <34580348+osamagmt@users.noreply.github.com>
2019-04-26 11:14:54 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,37 +1,30 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefixac = "";
-client.on('message', message => {
- let rebel = message.content.split(` `).slice(1).join(' ');
-if(message.author.id !== "460976885036220426") return ;
-if (message.content.startsWith(prefixac + 'setp')) {
-if(!rebel) return message.channel.send(`**ex ? ${prefixac}setp Hi**`).then(m => m.delete(3000));
-client.user.setActivity(rebel,{ type: 'PLAYING' });
-message.channel.send("**تـــ تغيير الحالة ــم**");} 
-if (message.content.startsWith(prefixac + 'setl')) {
-if(!rebel) return message.channel.send(`**ex ? ${prefixac}setl Hi**`).then(m => m.delete(3000));
-client.user.setActivity(rebel, { type: 'LISTENING' });
-message.channel.send("**تـــ تغيير الحالة ــم**");}
-if (message.content.startsWith(prefixac + 'sets')) {
-if(!rebel) return message.channel.send(`**ex ? ${prefixac}sets Hi**`).then(m => m.delete(3000));
-client.user.setActivity(rebel, { type: 'STREAMING', url:'https://twitch.tv/osamagmt' });
-message.channel.send("**تـــ تغيير الحالة ــم**");}     
-if (message.content.startsWith(prefixac + 'setw')) {
-if(!rebel) return message.channel.send(`**ex ? ${prefixac}setw Hi**`).then(m => m.delete(3000));
-client.user.setActivity(rebel, { type: 'WATCHING' });
-message.channel.send("**تـــ تغيير الحالة ــم**");}  
-});
+const adminprefix = "";
+const developers = ["460976885036220426" , ""];
+client.on('ready', () => {
+    console.log(`Logged as ${client.user.tag}`)
+})
 
 client.on('message', message => {
-	if (message.author.id = '460976885036220426') {
-		const args = message.content.slice(prefixac.length).trim().split(' ')
-		if(message.content.startsWith(`${prefixac}avatar`)) {
-			client.user.setAvatar(args[1]).then(async () => {
-				await message.reply(client.user.displayAvatarURL)
-			})
-			}
-	}
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.send(``)
+  } else 
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(``)
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(``)
+  } else
+  if (message.content.startsWith(adminprefix1 + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/osama_gmt");
+      message.channel.send(``)
+  }
 });
-
 
 client.login(process.env.BOT_TOKEN);
