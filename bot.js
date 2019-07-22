f4de4136c545ee56459f65001cc62b9257331264
osamagmt <34580348+osamagmt@users.noreply.github.com>
2019-07-22 15:39:19 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ client.on('message', message => {
   client.user.setActivity(argresult , {type:'LISTENING'});
       message.channel.send(``)
   } else
-  if (message.content.startsWith(adminprefix1 + 'st')) {
+  if (message.content.startsWith(adminprefix + 'st')) {
     client.user.setGame(argresult, "https://www.twitch.tv/osama_gmt");
       message.channel.send(``)
   }
